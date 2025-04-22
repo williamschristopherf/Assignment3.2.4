a3fe999f4ba451b3006082cbc4bb68f14bc3c5a4
Christopher Williams <williamstophef@gmail.com>
2025-04-21 20:50:40 -0700

    Comments added

--- Program.cs
+++ Program.cs
@@ -3,12 +3,14 @@ double sum = 0;
 
 Console.WriteLine("Enter the four numbers.>>> ");
 
+//for loop needed for input to myArray
 for (int i = 0; i < 4; i++)
 {
     Console.WriteLine($"Number {i + 1}");
     myArr[i] = Convert.ToDouble(Console.ReadLine());
 }
 
+//output for the array
 Console.Write("[");
 for (int i = 0; i < 4; i++)
 {
@@ -19,7 +21,7 @@ Console.Write("]");
 
 Console.WriteLine();
 
-
+//method with return type so it can return 2 values instead of 1: the sum and the average
 double[] SumArr(double[] myArr)
 {
     double[] sumAverage = new double[2];
@@ -31,6 +33,7 @@ double[] SumArr(double[] myArr)
     sumAverage[1] = sum / myArr.Length;
     return sumAverage;
 }
+//had to set a separate variable becuase it was doubling the second call in the second writeline
 double[] result = SumArr(myArr);
 Console.WriteLine($"The sum is {result[0]}");
 Console.WriteLine($"The average is {result[1]}");
